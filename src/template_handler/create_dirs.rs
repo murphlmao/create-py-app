84a0044d19a186aac595dd3395612b84688006fa
murphlmao <murphyjmalcolm@gmail.com>
2024-10-24 01:22:19 -0400

    feat: creating directories for template

--- src/template_handler/create_dirs.rs
+++ src/template_handler/create_dirs.rs
@@ -1,2 +1,68 @@
-// TODO: need to make method in each template handler file w/ list of required directories
-// - call that, append to existing array, then create all directories
+// libraries
+
+// modules
+
+
+pub struct DirectoryManager {
+    pub scripts: Vec<String>,
+    pub docs: Vec<String>,
+    pub project: Vec<String>,
+    pub docker: Vec<String>,
+    pub git: Vec<String>,
+}
+
+impl DirectoryManager {
+    pub fn new(base_path: &str) -> Self {
+        fn prefix_paths(base: &str, paths: Vec<&str>) -> Vec<String> {
+            paths.into_iter()
+                .map(|p| format!("{}/{}", base.trim_end_matches('/'), p.trim_start_matches("./")))
+                .collect()
+        }
+
+        DirectoryManager {
+            scripts: prefix_paths(base_path, vec![
+                "./scripts/",
+                "./scripts/cd",
+                "./scripts/ci",
+                "./scripts/dev",
+            ]),
+            docs: prefix_paths(base_path, vec![
+                "./docs",
+                "./docs/Usage",
+            ]),
+            project: prefix_paths(base_path, vec![
+                "./src/project",
+                "./src/project/config",
+                "./src/project/exceptions",
+            ]),
+            docker: prefix_paths(base_path, vec![
+                "./docker/dev",
+                "./docker/prod",
+            ]),
+            git: prefix_paths(base_path, vec![
+                "./.gitlab",
+                "./.gitlab/issue_templates/",
+                "./.gitlab/merge_request_templates/",
+            ]),
+        }
+    }
+
+    fn directories_to_create(&self) -> Vec<String> {
+        let mut all_dirs = Vec::new();
+
+        all_dirs.extend(self.scripts.clone());
+        all_dirs.extend(self.docs.clone());
+        all_dirs.extend(self.project.clone());
+        all_dirs.extend(self.docker.clone());
+        all_dirs.extend(self.git.clone());
+
+        return all_dirs
+    }
+
+    pub fn create(&self) -> () {
+        let dirs = self.directories_to_create();
+        for dir in dirs {
+            std::fs::create_dir_all(dir).unwrap();
+        }
+    }
+}
